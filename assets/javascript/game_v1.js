aaaa4ab8aba37e6d2207389ea335d9299c82f555
Damien Wright <damien@Damiens-MacBook-Pro.local>
2016-07-16 19:15:41 -0700

    rewrote game.js to include full words

--- assets/javascript/game_v1.js
+++ assets/javascript/game_v1.js
@@ -1,32 +1,26 @@
 
-var word = ['charizard', 'mewtwo', 'lugia', 'blastoise', 'dragonite', 'pikachu', 'gengar'];
+var word = ['cat', 'dog', 'monkey'];
 var wins = 0;
 var losses = 0;
 var guessesLeft = 5;
 var wrongLetters = [];
-var correctLetters = [];
-
-var randomWord = word[Math.floor(Math.random() * word.length)];
-console.log('The answer is ' + randomWord);
 
 document.onkeyup = function(event) {
 	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
-	if (randomWord.indexOf(userGuess) >= 0) {
+	if (word.indexOf(userGuess) >= 0) {
 		console.log('You guessed: ' + userGuess);
-		correctLetters.push(userGuess);
 		console.log('Correct');
 
 	} else {
 		wrongLetters.push(userGuess);
+		console.log(wrongLetters);
 		console.log('Nah dawg, you guessed ' + userGuess);
 		console.log(guessesLeft--);
 
 	}
 
-console.log(correctLetters);
-
-	score = "**SCORE**" + "<div id = 'wins'>" + 'WINS: ' + wins +
+	score = "~SCORE~" + "<div id = 'wins'>" + 'WINS: ' + wins +
 	"<div id = 'losses'>" + 'LOSSES: ' + losses;
 	document.querySelector('#game').innerHTML = score;
 
