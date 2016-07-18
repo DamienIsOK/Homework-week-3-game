fb6dc92dc4ab0e2b72600bac44f7048fe39d11fc
Damien Wright <damien@Damiens-MacBook-Pro.local>
2016-07-18 13:29:20 -0700

    the dashes now work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,39 +2,63 @@
 var word = ['charizard', 'mewtwo', 'lugia', 'blastoise', 'dragonite', 'pikachu', 'gengar'];
 var wins = 0;
 var losses = 0;
-var guessesLeft = 5;
+var attemptsLeft = 10;
+var dash = "_ ";
+var letters = "";
 var wrongLetters = [];
 var correctLetters = [];
 
+
 var randomWord = word[Math.floor(Math.random() * word.length)];
-console.log('The answer is ' + randomWord);
+console.log('The correct answer is ' + randomWord);
+
+// word is a string
+// letters in an array
+// dash is an optional string
+// letters = ["p","i","k","a","c","h","u"]
+
+var dashWord = function(word,letters){
+	// dash = dash || "_";
+	var output = "";
+	for(var i = 0; i < randomWord.length; i++){
+		if(letters.indexOf(word[i]) > -1){
+			output = output + word[i];
+		}
+		else
+		{
+			output = output + dash;
+		}
+	}
+
+	return output;
+}
 
 document.onkeyup = function(event) {
 	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
 	if (randomWord.indexOf(userGuess) >= 0) {
-		console.log('You guessed: ' + userGuess);
+		console.log('Correct! You guessed: ' + userGuess);
 		correctLetters.push(userGuess);
-		console.log('Correct');
+
 
 	} else {
 		wrongLetters.push(userGuess);
-		console.log('Nah dawg, you guessed ' + userGuess);
-		console.log(guessesLeft--);
+		console.log('Nah dawg, that\'s wrong. You guessed: ' + userGuess);
+		attemptsLeft--;
 
 	}
-
-console.log(correctLetters);
-
-	score = "**SCORE**" + "<div id = 'wins'>" + 'WINS: ' + wins +
+	
+	score = "<div id = 'wins'>" + 'WINS: ' + wins +
 	"<div id = 'losses'>" + 'LOSSES: ' + losses;
 	document.querySelector('#game').innerHTML = score;
 
-	letters = "<div id = 'guessedLetters'>" + wrongLetters + ' ';
+	letters = "<div id = 'guessedLetters'>" + 'Guessed: ' + wrongLetters + ' ';
 	document.querySelector('#guessedLetterBox').innerHTML = letters;
 
-	guesses = "<div id = 'guessCount'>" + 'GUESSES REMAINING: ' + guessesLeft;
+	guesses = "<div id = 'guessCount'>" + 'Attempts Left: ' + attemptsLeft;
 	document.querySelector('#guessCountBox').innerHTML = guesses;
 
+	dashedLetter = 	dashWord(randomWord,correctLetters);
+	document.querySelector('#dashedBox').innerHTML = dashedLetter;
 
-}
+};
